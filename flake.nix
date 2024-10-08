322b9b4ed3f0bea6ce84b2da546c88bd0779cf35
Tanel Dettenborn <tanel@ssrc.tii.ae>
2024-10-08 16:05:18 +0300

    Fixiso_minimal evaluation: OP-TEE option path wrong
    Signed-off-by: Tanel Dettenborn <tanel@ssrc.tii.ae>

--- flake.nix
+++ flake.nix
@@ -17,8 +17,8 @@
 
         hardware.nvidia-jetpack = {
           enable = true;
-          optee.fvForEKB = "a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 aa ab ac ad ae af";
-          optee.fvForSSK = "b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 ba bb bc bd be bf";
+          firmware.optee.fvForEKB = "a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 aa ab ac ad ae af";
+          firmware.optee.fvForSSK = "b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 ba bb bc bd be bf";
         };
       };
     in
